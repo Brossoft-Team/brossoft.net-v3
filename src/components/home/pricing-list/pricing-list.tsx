9f813f922e73bfc0c3b6283b67f4f9c39c8cc147
arda tor <arda.babathor@gmail.com>
2024-11-26 21:52:23 +0100

    refactor:pricing ui improved

--- src/components/home/pricing-list/pricing-list.tsx
+++ src/components/home/pricing-list/pricing-list.tsx
@@ -2,14 +2,14 @@ import React from "react";
 
 const Pricing = () => {
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 gap-24  mx-80 text-white">
+    <div className="grid grid-cols-1 md:grid-cols-2 gap-24  mx-48 text-white">
 
-      <div className="flex flex-col border border-gray-800 rounded-2xl p-20 bg-gradient-to-tr from-[#000] to-[#1e1e1e75]   shadow-md text-center">
+      <div className="flex flex-col border border-gray-800 rounded-2xl p-20 bg-gradient-to-tr from-[#000] to-[#1e1e1e] text-center">
         <h3 className="text-xl font-semibold tracking-wider mb-4">STANDARD PACKAGE</h3>
         <p className="text-sm text-gray-400 mb-2">Billed monthly</p>
-        <h2 className="text-3xl font-bold mb-6">$1,000</h2>
-        <ul className="flex-1 space-y-4 text-sm text-gray-300">
-          <li className="text-red-400">  <span className="text-red-600">✅</span> One request at a time</li>
+        <h2 className="text-3xl font-bold mb-6 tracking-wider ">$1,000</h2>
+        <ul className="flex-1 space-y-4 text-sm text-white">
+          <li className="">  <span className="text-red-600">✅</span> One request at a time</li>
           <li>✔️ Unlimited revisions</li>
           <li>✔️ 2 days average turnaround</li>
           <li>✔️ Design + development</li>
@@ -22,11 +22,11 @@ const Pricing = () => {
       </div>
 
       {/* Ⅰ ➊ ⓵ ✅  ✔️ ☑*/}
-      <div className="flex flex-col border border-gray-800 rounded-2xl p-20 bg-gradient-to-b from-[#0B0B0B] via-[#121212] to-[#1E1E1E] text-center">
+      <div className="flex flex-col border border-gray-800 rounded-2xl p-20 bg-gradient-to-tr from-[#000] to-[#1e1e1e] text-center">
         <h3 className="text-xl font-semibold tracking-wider mb-4">PRO PACKAGE</h3>
         <p className="text-sm text-gray-400 mb-2">Billed monthly</p>
-        <h2 className="text-3xl font-bold mb-6">$1,000</h2>
-        <ul className="flex-1 space-y-4 text-sm text-gray-300">
+        <h2 className="text-3xl font-bold mb-6">$3,000</h2>
+        <ul className="flex-1 space-y-4 text-sm text-white">
           <li>✔️ Two requests at a time</li>
           <li>✔️ Unlimited revisions</li>
           <li>✔️ 2 days average turnaround</li>
