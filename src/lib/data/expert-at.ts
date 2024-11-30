8637bec0e0482b56123d69608ba5dee53c629b36
Alp Emre Elmas <62440706+alpemreelmas@users.noreply.github.com>
2024-11-30 23:40:40 +0100

    feat: add mock data

---
+++ src/lib/data/expert-at.ts
@@ -0,0 +1,29 @@
+export const expertAt = [
+    {
+        title: "AI Dev",
+        description: "We are deeply passionate about designing custom-tailored solutions for our e-commerce clients.",
+        icon: "gear"
+    },
+    {
+        title: "UI Design",
+        description: "We push the boundaries of innovation through our creative UI designs, enabling you to distinguish yourself from competitors.",
+        icon: "design"
+    },
+    {
+        title: "Web Development",
+        description: "Our proficient team of frontend developers possesses a deep understanding of the latest frontend technologies and trends, including expertise in Webflow, Framer.",
+        icon: "web"
+    },
+    {
+        title: "Ecommerce",
+        description: "We are deeply passionate about designing custom-tailored solutions for our e-commerce clients.",
+        icon: "tag"
+    },
+    {
+        title: "Animation",
+        description: "We offer top-tier animations in various forms, such as frame by frame, stop motion, and motion graphics.",
+        icon: "animation"
+    }
+];
+
+export default expertAt;
